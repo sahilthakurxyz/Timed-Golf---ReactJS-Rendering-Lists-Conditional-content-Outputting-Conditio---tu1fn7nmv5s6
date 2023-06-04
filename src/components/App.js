eae9a5f509b6a6b7a54821d51078208413f6fcee
sahil thakur <sahilthakurxyz@gmail.com>
2023-06-05 00:01:37 +0530

    i am trying very hard to solve problemss

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,7 @@ const Timer = () => {
         break;
       case "ArrowDown":
         setY((prevY) => {
-          if (prevY + step < window.innerHeight - 20) {
+          if (prevY + step <= window.innerHeight - 20) {
             return prevY + step;
           } else {
             return prevY;
