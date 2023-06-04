8325620b45f0c15eb238dfa870c0db0c1efa70a1
sahil thakur <sahilthakurxyz@gmail.com>
2023-06-04 23:58:42 +0530

    i am trying very hard to solve problem

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const Timer = () => {
   let [y, setY] = useState(0);
   let [x, setX] = useState(0);
   const ballStyle = {
-    postion: "absolute",
+    position: "absolute",
     top: `${y}px`,
     left: `${x}px`,
     width: "20px",
@@ -15,12 +15,12 @@ const Timer = () => {
     borderRadius: "50%",
   };
   const holeStyle = {
-    postion: "absolute",
+    position: "absolute",
     top: "250px",
     left: "250px",
     width: "20px",
     height: "20px",
-    backGroundColor: "green",
+    backgroundColor: "green",
     borderRadius: "50%",
   };
   const handleKeyPress = (e) => {
@@ -34,29 +34,32 @@ const Timer = () => {
             return prevY;
           }
         });
-
         break;
       case "ArrowDown":
         setY((prevY) => {
-          if (prevY + step >= window.innerHeight - 20) {
-            return prevY;
-          } else {
+          if (prevY + step < window.innerHeight - 20) {
             return prevY + step;
+          } else {
+            return prevY;
           }
         });
         break;
       case "ArrowLeft":
         setX((prevX) => {
-          if (prevX - step < 0) {
+          if (prevX - step >= 0) {
+            return prevX - step;
+          } else {
             return prevX;
-          } else return prevX - step;
+          }
         });
         break;
       case "ArrowRight":
         setX((prevX) => {
-          if (prevX + step <= window.innerWidth) {
+          if (prevX + step <= window.innerWidth - 20) {
             return prevX + step;
-          } else return prevX;
+          } else {
+            return prevX;
+          }
         });
         break;
       default:
@@ -83,7 +86,7 @@ const Timer = () => {
       }
     };
     document.addEventListener("keydown", handleKeyDown);
-    return () => removeEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
   }, [gameStart]);
   useEffect(() => {
     if (x === 250 && y === 250) {
