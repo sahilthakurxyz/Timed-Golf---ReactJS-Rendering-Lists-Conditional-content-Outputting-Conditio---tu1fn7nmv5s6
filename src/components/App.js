1d91ca3109d0b09fdc45de652e07b4d7ad0868ed
sahil thakur <sahilthakurxyz@gmail.com>
2023-06-04 22:26:38 +0530

    i am trying very hard to solve problems

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,106 @@
-import React, { Component, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { time: 0, x: 0, y: 0 };
-  }
-  componentDidMount() {
-    
-  }
+const Timer = () => {
+  let [gameStart, setGameStart] = useState(false);
+  let [time, setTime] = useState(0);
+  let [y, setY] = useState(0);
+  let [x, setX] = useState(0);
+  const ballStyle = {
+    postion: "absolute",
+    top: `${y}px`,
+    left: `${x}px`,
+    width: "20px",
+    height: "20px",
+    backGroundColor: "red",
+    borderRadius: "50%",
+  };
+  const holeStyle = {
+    postion: "absolute",
+    top: "250px",
+    left: "250px",
+    width: "20px",
+    height: "20px",
+    backGroundColor: "green",
+    borderRadius: "50%",
+  };
+  const handleKeyPress = (e) => {
+    const step = 5;
+    switch (e.key) {
+      case "ArrowUp":
+        setY((prevY) => {
+          if (prevY - step >= 0) {
+            return prevY - step;
+          } else {
+            return prevY;
+          }
+        });
 
-  componentWillUnmount() {
-    
-  }
+        break;
+      case "ArrowDown":
+        setY((prevY) => {
+          if (prevY + step >= window.innerHeight - 20) {
+            return prevY;
+          } else {
+            return prevY + step;
+          }
+        });
+        break;
+      case "ArrowLeft":
+        setX((prevX) => {
+          if (prevX - step < 0) {
+            return prevX;
+          } else return prevX - step;
+        });
+        break;
+      case "ArrowRight":
+        setX((prevX) => {
+          if (prevX + step <= window.innerWidth) {
+            return prevX + step;
+          } else return prevX;
+        });
+        break;
+      default:
+        break;
+    }
+  };
 
+  useEffect(() => {
+    if (gameStart) {
+      const interval = setInterval(() => {
+        setTime((preTime) => preTime + 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [gameStart]);
+  const handleClickStart = () => {
+    setGameStart(true);
+  };
 
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (gameStart) {
+        handleKeyPress(e);
+      }
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => removeEventListener("keydown", handleKeyDown);
+  }, [gameStart]);
+  useEffect(() => {
+    if (x === 250 && y === 250) {
+      setGameStart(false);
+    }
+  }, [x, y]);
+  return (
+    <>
+      <button className="start" disabled={gameStart} onClick={handleClickStart}>
+        Start
+      </button>
+      <div className="heading-timer">{time}s</div>
 
-  render() {
-    return (
- <>
-</>
-    );
-  }
-}
+      <div className="ball" style={ballStyle}></div>
+      <div className="hole" style={holeStyle}></div>
+    </>
+  );
+};
 
 export default Timer;
