2020dcb94d9922ec7d9f3c9873d40455c8d8fb3e
sahil thakur <sahilthakurxyz@gmail.com>
2023-06-05 01:44:30 +0530

    i am trying hard to solve problems newton school

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "../styles/App.css";
-const timerr = () => {
+const App = () => {
   let [gameStart, setGameStart] = useState(false);
-  let [timer, settimer] = useState(0);
+  let [timer, setTimer] = useState(0);
   let [y, setY] = useState(0);
   let [x, setX] = useState(0);
+
   const ballStyle = {
     position: "absolute",
     top: `${y}px`,
     left: `${x}px`,
     width: "20px",
     height: "20px",
-    backGroundColor: "red",
+    backgroundColor: "red",
     borderRadius: "50%",
   };
+
   const holeStyle = {
     position: "absolute",
     top: "250px",
@@ -23,6 +25,7 @@ const timerr = () => {
     backgroundColor: "green",
     borderRadius: "50%",
   };
+
   const handleKeyPress = (e) => {
     const step = 5;
     switch (e.key) {
@@ -34,14 +37,13 @@ const timerr = () => {
             return prevY;
           }
         });
-
         break;
       case "ArrowDown":
         setY((prevY) => {
-          if (prevY + step >= window.innerHeight - 20) {
-            return prevY;
-          } else {
+          if (prevY + step <= window.innerHeight - 20) {
             return prevY + step;
+          } else {
+            return prevY;
           }
         });
         break;
@@ -49,14 +51,18 @@ const timerr = () => {
         setX((prevX) => {
           if (prevX - step >= 0) {
             return prevX - step;
-          } else return prevX;
+          } else {
+            return prevX;
+          }
         });
         break;
       case "ArrowRight":
         setX((prevX) => {
           if (prevX + step <= window.innerWidth - 20) {
             return prevX + step;
-          } else return prevX;
+          } else {
+            return prevX;
+          }
         });
         break;
       default:
@@ -67,13 +73,17 @@ const timerr = () => {
   useEffect(() => {
     if (gameStart) {
       const interval = setInterval(() => {
-        settimer((pretimer) => pretimer + 1);
+        setTimer((prevTimer) => prevTimer + 1);
       }, 1000);
       return () => clearInterval(interval);
     }
   }, [gameStart]);
+
   const handleClickStart = () => {
     setGameStart(true);
+    setTimer(0);
+    setX(0);
+    setY(0);
   };
 
   useEffect(() => {
@@ -85,21 +95,22 @@ const timerr = () => {
     document.addEventListener("keydown", handleKeyDown);
     return () => document.removeEventListener("keydown", handleKeyDown);
   }, [gameStart]);
+
   useEffect(() => {
     if (x === 250 && y === 250) {
       setGameStart(false);
     }
   }, [x, y]);
+
   return (
     <>
       <button className="start" disabled={gameStart} onClick={handleClickStart}>
         Start
       </button>
-      <div className="heading-timer">{timer}</div>
+      <div className="heading-timer">{timer}s</div>
       <div className="ball" style={ballStyle}></div>
       <div className="hole" style={holeStyle}></div>
     </>
   );
 };
-
-export default timerr;
+export default App;
